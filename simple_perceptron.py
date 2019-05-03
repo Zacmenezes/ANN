ac1b5f09a10c023ca9decdd1e5cc0e3c1674e32f
isaac <isaac.menezes07@gmail.com>
2019-05-02 21:10:16 -0300

    Creating hit_count method

--- simple_perceptron.py
+++ simple_perceptron.py
@@ -1,15 +1,13 @@
 import numpy as np
 import pandas as pd
 
-# Make a prediction with weights
 def predict(row, weights):
     activation = np.dot(row, weights)
     return 1 if activation >= 0.0 else 0
 
 def train_weights(train, l_rate, n_epoch):
     train_values = train.values
-    weights = np.ones(len(train.columns) - 2)
-    weights = np.append(-1, weights)
+    weights = np.append( -1 , np.ones(len(train.columns) - 2))
     for epoch in range(n_epoch):
         for row in train_values:
             prediction = predict(row[:-1], weights)
@@ -20,26 +18,27 @@ def train_weights(train, l_rate, n_epoch):
 def create_x0(data):
     return pd.DataFrame({'x0': np.ones(len(data.index))})
 
-# Preparing iris dataset
-iris_df = pd.read_csv("data/iris.data", header=None)
-iris_df.columns = ['x1', 'x2', 'x3', 'x4', 'd']
-iris_df['d'] = iris_df['d'].map({'Iris-setosa': 0,'Iris-versicolor': 1, 'Iris-virginica': 1})
-iris_df['label'] = iris_df['d'].map({0 : 'Setosa', 1 : 'Other'})
-x0 = create_x0(iris_df)
-iris_df = x0.join(iris_df)
+def prepare_dataset(path):
+    df = pd.read_csv(path, header=None)
+    df.columns = ['x1', 'x2', 'x3', 'x4', 'd']
+    df['d'] = df['d'].map({'Iris-setosa': 1,'Iris-versicolor': 0, 'Iris-virginica': 0})
+    x0 = create_x0(df)
+    df = x0.join(df)
+    return df
 
-# Split between train and test
-train=iris_df.sample(frac=0.8,random_state=250)
+def hit_rate(data):
+    hit = 0
+    for row in data:
+        actual = row[-1]
+        prediction = predict(row[:-1], w)
+        if actual == prediction:
+            hit += 1
+        print("Expected=%d, Predicted=%d" % (row[-1], prediction))
+    return hit / float(len(data)) * 100.0
+
+iris_df = prepare_dataset("data/iris.data")
+train=iris_df.sample(frac=0.8)
 test=iris_df.drop(train.index)
+w = train_weights(train, 0.1, 20)
 
-train = train.drop(columns=['label'])
-
-w = train_weights(train, 0.2, 20)
-
-# print(test.values)
-for row in test.values:
-	prediction = predict(row[:-2], w)
-	print("Expected=%d, Predicted=%d" % (row[-2], prediction))
-
-# x = predict([1, 5.3,3.7,1.5,0.2], w)
-# print(x)
+print(hit_rate(test.values))
