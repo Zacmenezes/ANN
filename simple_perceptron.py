0ea9f67e35534610d2e1fda144702f4511adaac2
isaac <isaac.menezes07@gmail.com>
2019-05-02 23:46:08 -0300

    Creating a method to evaluate the model accuracy

--- simple_perceptron.py
+++ simple_perceptron.py
@@ -7,7 +7,7 @@ def predict(row, weights):
 
 def train_weights(train, l_rate, n_epoch):
     train_values = train.values
-    weights = np.append( -1 , np.ones(len(train.columns) - 2))
+    weights = np.append(-1 , np.ones(len(train.columns) - 2))
     for epoch in range(n_epoch):
         for row in train_values:
             prediction = predict(row[:-1], weights)
@@ -18,27 +18,38 @@ def train_weights(train, l_rate, n_epoch):
 def create_x0(data):
     return pd.DataFrame({'x0': np.ones(len(data.index))})
 
-def prepare_dataset(path):
+def prepare_dataset(path, index):
     df = pd.read_csv(path, header=None)
     df.columns = ['x1', 'x2', 'x3', 'x4', 'd']
-    df['d'] = df['d'].map({'Iris-setosa': 1,'Iris-versicolor': 0, 'Iris-virginica': 0})
+    df['d'] = np.where(df['d']==index, 0, 1)
     x0 = create_x0(df)
     df = x0.join(df)
     return df
 
-def hit_rate(data):
+def hit_rate(data, trained_weights):
     hit = 0
     for row in data:
         actual = row[-1]
-        prediction = predict(row[:-1], w)
+        prediction = predict(row[:-1], trained_weights)
         if actual == prediction:
             hit += 1
-        print("Expected=%d, Predicted=%d" % (row[-1], prediction))
+        # print("Expected=%d, Predicted=%d" % (row[-1], prediction))
     return hit / float(len(data)) * 100.0
 
-iris_df = prepare_dataset("data/iris.data")
-train=iris_df.sample(frac=0.8)
-test=iris_df.drop(train.index)
-w = train_weights(train, 0.1, 20)
+def accuracy(data):
+    results = []
+    for i in range(20):
+        train=data.sample(frac=0.8)
+        test=data.drop(train.index)
+        w = train_weights(train, 0.45, 20)
+        results.append(hit_rate(test.values, w))
+    # print(results)
+    return np.average(results)
 
-print(hit_rate(test.values))
+setosa_df = prepare_dataset("data/iris.data", "Iris-setosa")
+versicolor_df = prepare_dataset("data/iris.data", "Iris-versicolor")
+virginica_df = prepare_dataset("data/iris.data", "Iris-virginica")
+
+print(accuracy(setosa_df))
+print(accuracy(versicolor_df))
+print(accuracy(virginica_df))
