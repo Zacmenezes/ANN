79a205ed9c1c1a45e5f6e216ca06420fd0f77a29
isaac <isaac.menezes07@gmail.com>
2019-05-22 21:50:47 -0300

    plotting 3d

--- adaline.py
+++ adaline.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from toy_problem_adaline import Toy2
+from mpl_toolkits.mplot3d import Axes3D as a3d
 
 class Adaline:
     
@@ -32,8 +33,19 @@ class Adaline:
     def realization(self):
         train, test = train_test_split(self.problem.data, test_size=0.2)
         weights = self.train_weights(train, self.l_rate, self.n_epoch)
-        predictions = [self.predict(row[:-1], weights) for row in test.values]
+        predictions = []
+        errors = []
+        for row in test.values:
+            prediction = self.predict(row[:-1], weights)
+            predictions.append(prediction)
+            error = row[-1] - prediction
+            errors.append(error * error)
+        print("MSE=%f" % np.mean(errors))
         self.plot(test, predictions)
+
+    def evaluate(self, n):
+        for _ in range(n):
+            self.realization()
 
     def plot(self, data, predictions):
         if(self.problem.c == None):
@@ -41,8 +53,19 @@ class Adaline:
                 plt.scatter(row[1], row[2], c='blue', s=10)
                 plt.scatter(row[1], predictions[index], c='red', s=10)
             plt.show()
+        else:
+            z_surface = []
+            for index, row in enumerate(data.values):
+                z_surface.append(predictions[index])
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot_trisurf(data['x'].values, data['y'].values, z_surface, linewidth=1, antialiased=True)
+            ax.scatter(data['x'].values, data['y'].values, data['z'].values, c='red', marker='o')
+            plt.show()
+
             
 
-toy = Toy2(-4,1)
-a = Adaline(toy, 0.01, 200)
+toy = Toy2(4, 8 ,-5)
+a = Adaline(toy, 0.01, 1000)
 a.realization()
+# a.evaluate(20)
--- toy_problem_adaline.py
+++ toy_problem_adaline.py
@@ -17,7 +17,7 @@ class Toy2(AbstractProblem):
         return self.create_2d(X)
         
     def create_2d(self, X):
-        Y = [(self.a * x + self.b) + np.random.uniform(-self.a/2,self.a/2) for x in X]
+        Y = [(self.a * x + self.b) + np.random.uniform(-1,1) for x in X]
         df = pd.DataFrame(np.array([[i,j] for i,j in zip(X,Y)]))
         df.columns = ['x','y']
         df = self.normalize(df)
@@ -27,7 +27,7 @@ class Toy2(AbstractProblem):
 
     def create_3d(self, X):
         Y = np.random.rand(100, 1)
-        Z = [(self.a * x + self.b * y + self.c) + np.random.uniform(-1, 1) for x, y in zip(X, Y)]
+        Z = [(self.a * x + self.b * y + self.c) + np.random.uniform(-0.1, 0.1) for x, y in zip(X, Y)]
         df = pd.DataFrame(np.array([[Z[i][0], X[i], Y[i][0]] for i in range(len(Z))]))
         df.columns = ['x','y','z']
         df = self.normalize(df)
