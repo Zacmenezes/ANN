1d511dbd280526dd2deaed04a588e67fd9a8fe2b
isaac <isaac.menezes07@gmail.com>
2019-05-25 13:42:24 -0300

    ploting decision surface

--- single_layer_perceptron.py
+++ single_layer_perceptron.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from iris_problem import Iris
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
 
 class SingleLayerPerceptron():
  
@@ -14,12 +16,13 @@ class SingleLayerPerceptron():
     def train_weights(self, train_df, learn_rate, max_epochs):
         train_data = train_df.drop(['d0','d1','d2'], axis=1).values
         target = train_df[['d0','d1','d2']].values
-        weights = np.random.randn(3,5)
+        weights = np.random.randn(self.neurons, train_data.shape[1])
         epoch = 0
+
         while(epoch < max_epochs):
-            # np.random.shuffle(train_data)
+            shuffle(train_data, target)
             for row, row_target in zip(train_data, target):  
-                predictions = self.validate(np.dot(row, weights.T))
+                predictions = self.predict(row, weights.T)
                 error = row_target - predictions
                 weights = weights + learn_rate * np.outer(row, error).T
             epoch += 1
@@ -40,7 +43,11 @@ class SingleLayerPerceptron():
         train, test = train_test_split(self.problem.data, test_size=0.2)
         weights = self.train_weights(train, self.learn_rate, self.max_epochs)
         print(self.hit_rate(test, weights))
+        self.plot_decision_surface(test, weights)
     
+    def predict(self, row, weights):
+        return self.validate(np.dot(row, weights))
+
     def validate(self, prediction):
         degrau = np.vectorize(lambda x: 1 if x >= 0 else 0)
         if(sum(degrau(prediction)) != 1):
@@ -48,7 +55,36 @@ class SingleLayerPerceptron():
         else:
             return degrau(prediction)
 
+    def plot_decision_surface(self, data, weights):
+        c = data.columns
+        actual = data[['d0','d1','d2']].values
+        test = data.drop(['d0','d1','d2'], axis=1).values
 
-iris = Iris()
+        x1_max, x1_min = data[c[1]].max() + 0.2, data[c[1]].min() - 0.2
+        x2_max, x2_min = data[c[2]].max() + 0.2, data[c[2]].min() - 0.2
+
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.1), np.arange(x2_min, x2_max, 0.1))
+        Z =  np.array([xx1.ravel(), xx2.ravel()]).T
+        
+        fig, ax = plt.subplots()
+        for x1, x2 in Z:
+            if (self.predict([1, x1, x2], weights.T) == np.array([1, 0, 0])).all(): 
+                ax.scatter(x1, x2, c='red', s=1.5, marker='o')
+            elif (self.predict([1, x1, x2], weights.T) == np.array([0, 1, 0])).all():
+                ax.scatter(x1, x2, c='blue', s=1.5, marker='o')
+            elif (self.predict([1, x1, x2], weights.T) == np.array([0, 0, 1])).all(): 
+                ax.scatter(x1, x2, c='green', s=1.5, marker='o')
+
+        for row, row_target in zip(test, actual):
+            if (row_target == np.array([1, 0, 0])).all():
+                ax.scatter(row[1], row[2], c='red', marker='v')
+            elif (row_target == np.array([0, 1, 0])).all():
+                ax.scatter(row[1], row[2], c='blue', marker='*')       
+            elif (row_target == np.array([0, 0, 1])).all():
+                ax.scatter(row[1], row[2], c='green', marker='o')       
+        plt.show()
+
+
+iris = Iris(drop=['x1', 'x2'])
 slp  = SingleLayerPerceptron(problem=iris, learn_rate=0.01, max_epochs=500)
 slp.test()
